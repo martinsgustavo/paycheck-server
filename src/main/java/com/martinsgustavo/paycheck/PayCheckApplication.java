3b67f6fe4f57d223bbdd620503125ff97acebcca
Gustavo Martins <martinsgustavox@gmail.com>
2020-01-18 00:05:04 -0500

    project structure

---
+++ src/main/java/com/martinsgustavo/paycheck/PayCheckApplication.java
@@ -0,0 +1,13 @@
+package com.martinsgustavo.paycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martinsgustavo/paycheck/PayCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinsgustavo.paycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
